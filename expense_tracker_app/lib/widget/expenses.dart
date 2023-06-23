4ca153898037fa9afeffaf75b3ebcf712ec5fe88
渡邉　晴哉 <watanabeha@systena.co.jp>
2023-06-24 08:20:01 +0900

    Flutter & Dart - The Complete Guide [2023 Edition] #107

---
+++ expense_tracker_app/lib/widget/expenses.dart
@@ -0,0 +1,60 @@
+import 'package:expense_tracker_app/widget/expenses_list/expenses_list.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/widget/expenses_list/new_expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registerExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  void _openAssExprnseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => NewExpense(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAssExprnseOverlay,
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registerExpenses),
+          )
+        ],
+      ),
+    );
+  }
+}
